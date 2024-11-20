14b3c1470dd74bd33c9d2db0fd1c7a234e5d1cbd
Zoltan Radics <zoltan.radics@adnovum.hu>
2024-11-21 00:24:08 +0100

    Introduce Head component

---
+++ src/lib/components/Head.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Status from '$lib/components/Status.svelte';
+	import { EpisodeStatus } from '$lib/types/episode';
+	import TimestampDisplay from '$lib/components/TimestampDisplay.svelte';
+	import DurationDisplay from '$lib/components/DurationDisplay.svelte';
+
+	interface HeadProps {
+		status: EpisodeStatus;
+		onAirTime: number;
+		offAirTime: number;
+	}
+
+	const { status, onAirTime, offAirTime }: HeadProps = $props();
+</script>
+
+<div class="w-full border-b border-gray-300">
+	<div class="m-3 flex">
+		<div class="mr-2 border-r-2 border-gray-200 pr-2">
+			<i class="fa-solid fa-arrow-left"></i>
+		</div>
+		<div class="mr-5">
+			<Status {status} />
+		</div>
+		<div class="mr-5">
+			<TimestampDisplay color="green" value={onAirTime} />
+		</div>
+		<div class="mr-5">
+			<TimestampDisplay color="red" value={offAirTime} />
+		</div>
+		<div class="mr-3">
+			<DurationDisplay {onAirTime} {offAirTime} />
+		</div>
+	</div>
+</div>
