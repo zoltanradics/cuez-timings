feb7deb7b7139aab48758c5840a470972b4da9b9
Zoltan Radics <zoltan.radics@adnovum.hu>
2024-11-20 21:45:27 +0100

    Add routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import '@fortawesome/fontawesome-free/css/all.min.css';
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import Head from '$lib/components/Head.svelte';
+	import Rundown from '$lib/components/Rundown.svelte';
+	import { EpisodeStatus } from '$lib/types/episode';
+
+	import episodeData from '../data/episode.json';
+	import timingsData from '../data/timings.json';
+
+	const status = episodeData.episode.status as EpisodeStatus;
+	const onAirTime = timingsData.episode.on_air_time;
+	const offAirTime = timingsData.episode.off_air_time;
+</script>
+
+<Head {status} {onAirTime} {offAirTime} />
+<Rundown {episodeData} {timingsData} />
