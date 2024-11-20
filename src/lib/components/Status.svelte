fb3c6d7d2ad679f213d6ca7db2d318838f62fade
Zoltan Radics <zoltan.radics@adnovum.hu>
2024-11-21 00:23:27 +0100

    Introduce Status component

---
+++ src/lib/components/Status.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { EpisodeStatus } from '$lib/types/episode';
+
+	const { status }: { status: EpisodeStatus } = $props();
+
+	function displayStatus(status: string): string {
+		switch (status) {
+			case EpisodeStatus.ACTIVE:
+				return 'Live';
+			default:
+				return status;
+		}
+	}
+</script>
+
+<div class="px-4">
+	{displayStatus(status)}
+	<i class="fa-solid fa-chevron-down ml-1 text-sm"></i>
+</div>
