e10bb0d74fd1c8f1cc3722e6c7693946ea237af8
Zoltan Radics <zoltan.radics@adnovum.hu>
2024-11-21 00:22:32 +0100

    Introduce Timestamp related components

---
+++ src/lib/components/Timestamp.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	interface TimestampProps {
+		value: number;
+	}
+
+	const {value }: TimestampProps = $props();
+	const formattedTime = new Date(value * 1000).toLocaleString('be-BE', {
+		hour: '2-digit',
+		minute: '2-digit',
+		hour12: false
+	});
+</script>
+
+<span>{formattedTime}</span>
---
+++ src/lib/components/TimestampDisplay.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Timestamp from "./Timestamp.svelte";
+
+	interface TimestampDisplaypProps {
+		value: number;
+		color: 'red' | 'green'
+	}
+
+	const { value, color }: TimestampDisplaypProps = $props();
+</script>
+
+<i class="fa-solid fa-flag mr-2" class:text-red-500={color === 'red'} class:text-green-500={color === 'green'} ></i>
+<Timestamp value={value} />
