40a78affef80a23025949b750c0844b21ffdd16e
Zoltan Radics <zoltan.radics@adnovum.hu>
2024-11-21 00:23:00 +0100

    Introduce Duration related components

---
+++ src/lib/components/Duration.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { formatDuration } from '$lib/helpers/timings';
+
+	interface DurationProps {
+		value: number;
+	}
+
+	const { value }: DurationProps = $props();
+	const formattedDuration = formatDuration(value);
+</script>
+
+<span>{formattedDuration}</span>
---
+++ src/lib/components/DurationDisplay.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { calculateDifferenceBetweenTimestamps } from '$lib/helpers/timings';
+	import Duration from '$lib/components/Duration.svelte';
+
+	interface DurationDisplayProps {
+		onAirTime: number;
+		offAirTime: number;
+	}
+
+	const { onAirTime, offAirTime }: DurationDisplayProps = $props();
+	const durationDifference = calculateDifferenceBetweenTimestamps(onAirTime, offAirTime);
+</script>
+
+<div>
+	<i class="fa-solid fa-stopwatch mr-2"></i>
+	<Duration value={durationDifference} /> 
+</div>
