27501a01bd22767636d7bffd36c7d7fd7a4190d5
Zoltan Radics <zoltan.radics@adnovum.hu>
2024-11-21 22:18:41 +0100

    Introduce RundownHead

---
+++ src/lib/components/RundownHead.svelte
@@ -0,0 +1,10 @@
+<div class="rundown__head flex border-b border-gray-300 bg-white font-semibold text-stone-800">
+	<div class="index p-2"></div>
+	<div class="label p-2"></div>
+	<div class="title border-r border-gray-300 p-2"></div>
+	<div class="duration border-r border-gray-300 p-2 text-right">Est. Duration</div>
+	<div class="front-time border-r border-gray-300 p-2 text-right">Front Time</div>
+	<div class="end-time border-r border-gray-300 p-2 text-right">End Time</div>
+	<div class="back-time border-r border-gray-300 p-2 text-right">Back Time</div>
+	<div class="add border-r border-gray-300 p-2 text-right"><i class="fa-solid fa-plus"></i></div>
+</div>
