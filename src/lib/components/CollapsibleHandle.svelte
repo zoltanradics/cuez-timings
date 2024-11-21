ee7be4e955c831af3d6609428582e471f135b78b
Zoltan Radics <zoltan.radics@adnovum.hu>
2024-11-21 11:20:09 +0100

    Introeduce CollapsibleHandle component

---
+++ src/lib/components/CollapsibleHandle.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	interface CollapsibleHandleProps {
+		open: boolean;
+		label: string;
+	}
+
+	const { open, label }: CollapsibleHandleProps = $props();
+</script>
+
+<span class="mr-2 text-xs">
+	<i class="fa-solid" class:fa-caret-down={open} class:fa-caret-right={!open}></i>
+</span>
+<span class="font-semibold">{label}</span>
