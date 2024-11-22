28ee3949d2e379ee28e4bb867e3679437f48a7e8
Zoltan Radics <zoltan.radics@adnovum.hu>
2024-11-22 13:42:10 +0100

    Implement timings calculations

---
+++ src/lib/helpers/timings.spec.ts
@@ -0,0 +1,44 @@
+import { describe, it, expect } from 'vitest';
+import {
+	calculateDifferenceBetweenTimestamps,
+	convertMilisecondToSecond,
+	formatDuration
+} from './timings';
+
+describe('calculateDifferenceBetweenTimestamps', () => {
+	it('Checing positive difference', () => {
+		expect(calculateDifferenceBetweenTimestamps(0, 100)).toBe(100);
+	});
+
+	it('Checing negative difference', () => {
+		expect(calculateDifferenceBetweenTimestamps(100, 0)).toBe(100);
+	});
+
+	it('Checing zero difference', () => {
+		expect(calculateDifferenceBetweenTimestamps(0, 0)).toBe(0);
+	});
+});
+
+describe('convertMilisecondToSecond', () => {
+	it('Convert 1000ms to 1sec', () => {
+		expect(convertMilisecondToSecond(1000)).toBe(1);
+	});
+});
+
+describe('formatDuration', () => {
+	it('Checking one second', () => {
+		expect(formatDuration(1)).toBe('00:00:01');
+	});
+
+	it('Checking one minute', () => {
+		expect(formatDuration(60)).toBe('00:01:00');
+	});
+
+	it('Checking one hour', () => {
+		expect(formatDuration(3600)).toBe('01:00:00');
+	});
+
+	it('Checking one hour one minute one second', () => {
+		expect(formatDuration(3661)).toBe('01:01:01');
+	});
+});
---
+++ src/lib/helpers/timings.ts
@@ -0,0 +1,130 @@
+import type { EpisodeData } from '$lib/types/episode';
+import type { TimingsData } from '$lib/types/timings';
+import type { KeyValueMap, TimingProperties } from '$lib/types/shared';
+
+/**
+ *
+ * @param {number} start
+ * @param {number} end
+ * @returns {number}
+ */
+export function calculateDifferenceBetweenTimestamps(start: number, end: number): number {
+	return Math.abs(end - start);
+}
+
+/**
+ *
+ * @param {number} ms
+ * @returns {number}
+ */
+export function convertMilisecondToSecond(ms: number): number {
+	return ms / 1000;
+}
+
+/**
+ *
+ * Function to format seconds into HH:MM:SS
+ *
+ * @param {number} seconds
+ * @returns {string}
+ */
+export function formatDuration(seconds: number): string {
+	const hours = Math.floor(seconds / 3600);
+	const minutes = Math.floor((seconds % 3600) / 60);
+	const remainingSeconds = Math.floor(seconds % 60);
+
+	return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+}
+
+export function calculateTimings(episodeData: EpisodeData, timingsData: TimingsData): TimingsData {
+	const calculatedPartFields = calculatePartFields(episodeData, timingsData);
+	const calculatedItemFields = calculateItemFields(episodeData, {
+		...timingsData,
+		part: calculatedPartFields
+	});
+
+	return {
+		episode: { ...timingsData.episode },
+		part: calculatedPartFields,
+		item: calculatedItemFields
+	};
+}
+
+function calculateItemFields(
+	episodeData: EpisodeData,
+	timingsData: TimingsData
+): KeyValueMap<TimingProperties> {
+	const items = {};
+
+	episodeData.episode.parts.forEach((partId) => {
+		const initialFrontTime = timingsData.part[partId].front_time;
+
+		episodeData.part[partId].items.forEach((itemId, index, array) => {
+			const estimatedDuration: number = convertMilisecondToSecond(
+				timingsData.item[itemId].estimated_duration
+			);
+			const previousItemId: string = array[index - 1];
+			const previousItem: TimingProperties = timingsData.item[previousItemId];
+			const frontTime: number =
+				index === 0
+					? initialFrontTime
+					: previousItem.front_time + convertMilisecondToSecond(previousItem.estimated_duration);
+			const endTime = frontTime + estimatedDuration;
+
+			items[itemId] = {
+				estimated_duration: estimatedDuration,
+				front_time: frontTime,
+				end_time: endTime,
+				back_time: frontTime
+			};
+		});
+	});
+
+	return items;
+}
+
+function calculatePartFields(
+	episodeData: EpisodeData,
+	timingsData: TimingsData
+): KeyValueMap<TimingProperties> {
+	return episodeData.episode.parts.reduce((acc, partId, index, array) => {
+		const initialOnAirTime: number = timingsData.episode.on_air_time;
+		const previousPartId: string = array[index - 1];
+		const previousPart: TimingProperties = acc[previousPartId];
+		const estimatedDuration: number = convertMilisecondToSecond(
+			timingsData.part[partId].estimated_duration
+		);
+		const frontTime: number =
+			index === 0
+				? initialOnAirTime
+				: previousPart.front_time + convertMilisecondToSecond(previousPart.estimated_duration);
+		const endTime: number = frontTime + estimatedDuration;
+
+		return {
+			...acc,
+			[partId]: {
+				estimated_duration: estimatedDuration,
+				front_time: frontTime,
+				end_time: endTime,
+				back_time: frontTime
+			}
+		};
+	}, {});
+}
+
+export function convertToViewModel(episodeData: any, timingsData: any) {
+	return episodeData.episode.parts.map((partId) => ({
+		title: episodeData.part[partId].title,
+		estimatedDuration: timingsData.part[partId].estimated_duration,
+		frontTime: timingsData.part[partId].front_time,
+		endTime: timingsData.part[partId].end_time,
+		backTime: timingsData.part[partId].back_time,
+		items: episodeData.part[partId].items.map((itemId) => ({
+			title: episodeData.item[itemId].title,
+			estimatedDuration: timingsData.item[itemId].estimated_duration,
+			frontTime: timingsData.item[itemId].front_time,
+			endTime: timingsData.item[itemId].end_time,
+			backTime: timingsData.item[itemId].back_time
+		}))
+	}));
+}
