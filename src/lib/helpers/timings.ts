094e9c0575f5693d1e0ef39f186602329ff6cd28
Zoltan Radics <zoltan.radics@adnovum.hu>
2024-11-22 14:24:55 +0100

    Duration shows hour only if duration is greater than an hour

--- src/lib/helpers/timings.ts
+++ src/lib/helpers/timings.ts
@@ -33,7 +33,9 @@ export function formatDuration(seconds: number): string {
 	const minutes = Math.floor((seconds % 3600) / 60);
 	const remainingSeconds = Math.floor(seconds % 60);
 
-	return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+	return hours < 0
+		? `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`
+		: `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
 }
 
 /**
